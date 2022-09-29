bc79e9595f54c8d9ceb3a7ec07f787fcce949f77
Stephen Lyons <55640562+stephenlyons18@users.noreply.github.com>
2022-09-29 05:03:32 +0000

    Update install-nix.sh

--- install-nix.sh
+++ install-nix.sh
@@ -7,5 +7,8 @@ nix-env -iA nixos.brave
 NIXPKGS_ALLOW_UNFREE=1 nix-env -iA nixos.discord
 nix-env -iA nixos.exa
 nix-env -iA nixos.starship
+mkdir -p ~/.config && touch ~/.config/starship.toml
+curl https://starship.rs/presets/toml/pastel-powerline.toml > ~/.config/starship.toml
+eval "$(starship init bash)"
 nix-env -iA nixos.gh
 gh auth login
